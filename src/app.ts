9f4b886a435ff3e31a1536bcfc6af9436766d894
Jenath <jenathkanagesan@gmail.com>
2022-10-05 14:36:44 +0100

    Add API Key

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import got from 'got'
 
-const key = "YOUR-KEY-HERE"
+const key = "20751907-9a70-44f2-90f0-a5396c53437f"
 const url = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${key}`
 
 const json = await got.get(url).json()
