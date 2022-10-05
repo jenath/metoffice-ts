b1af18291c07a8a069d637afc3556f6eccb108af
Jenath <jenathkanagesan@gmail.com>
2022-10-05 15:45:53 +0100

    access name

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,25 @@ import got from 'got'
 const key = "20751907-9a70-44f2-90f0-a5396c53437f"
 const url = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${key}`
 
-const json = await got.get(url).json()
+const json = await got.get(url).json() as SitelistResponse
+const locationNames = json.Locations.Location.map(location => location.name)
 
-console.log(JSON.stringify(json))
+console.log(locationNames.join("\n"))
+
+//console.log(JSON.stringify(json))
+
+interface Location{
+    elevation: string,
+    id: string,
+    latitude: string,
+    longitude: string,
+    name: string,
+    region: string,
+    unitaryAuthArea: string,
+}
+
+interface SitelistResponse{
+    Locations: {
+        Location: Location[]
+    }
+}
