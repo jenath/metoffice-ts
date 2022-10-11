f944e2d11cb3f2400a70d5b983696e71cf278dbe
Jenath <jenathkanagesan@gmail.com>
2022-10-11 15:29:23 +0100

    create weather interface

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,22 @@ import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
 const userInput = prompt('Which city would you like to know the weather for?');
-
 const key = '20751907-9a70-44f2-90f0-a5396c53437f';
-let chosen_city_id;
-
 const url = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${key}`;
-const url2 = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${chosen_city_id}?res=daily&key=${key}`;
-
 const json = await got.get(url).json() as SitelistResponse;
-// const json2 = await got.get(url2).json() as WeatherResponse; 
 
 const locations = json.Locations.Location.map(location => ({ 
     name: location.name,
     id: location.id
 }));
+
+const chosen_city = locations.find(val => val.name === userInput)
+const chosen_city_id = chosen_city?.id;
+
+const url2 = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${chosen_city_id}?res=daily&key=${key}`;
+
+
+const json2 = await got.get(url2).json() as WeatherResponse; 
 
 
 // Sitelist interface
@@ -38,16 +40,54 @@ interface SitelistResponse{
     }
 }
 
-locations.find(val => {
-    if (val.name === userInput) {
-        chosen_city_id = val.id;
-    };
-});
-
-
 // Weather interface
 
-// interface WeatherResponse{
-//     DV
-// }
+interface Param{
+    name: string,
+    units: string,
+    $: string,
+}
 
+interface Period{
+    type: string,
+    value: string,
+    Rep: Rep[],
+}
+
+interface Rep{
+    D: string,
+    Gn: string,
+    Hn: string,
+    PPd: string,
+    S: string,
+    V: string,
+    Dm: string,
+    FDm: string,
+    W: string,
+    U: string,
+    $: string,
+}
+
+interface WeatherResponse{
+    SiteRep: {
+        Wx: {
+            Param: Param[]
+        },
+        DV: {
+            dataDate: string,
+            type: string,
+            Location: {
+                i: string,
+                lat: string,
+                lon: string,
+                name: string,
+                country: string,
+                continent: string,
+                elevation: string,
+                Period: Period[],
+            },
+        },
+    },
+}
+
+console.log(json2);
